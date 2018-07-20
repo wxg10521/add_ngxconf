f3ac28b596e66e41da0705729f55eee1017ad685
root <root@10.11.3.131>
2018-07-20 14:28:04 +0800

    add portiner

--- add_conf.py
+++ add_conf.py
@@ -1,6 +1,7 @@
 #-*- coding:utf-8 -*-
 import os,re
-ngxconf_dir='/nginx/conf.d'
+#ngxconf_dir='/hdd1/nginx/conf.d'
+ngxconf_dir='/nginx'
 #domain='www.baidu.com'
 #iport='10.11.3.28:9999 10.11.3.22:3333 10.11.3.44:2222'
 class add_conf():
@@ -30,11 +31,21 @@ class add_conf():
 	lines.insert(2,"	server_name %s;" % domain)
 	lines.insert(7,"	proxy_pass  %s;" % proxy_name)	
 	lines.insert(14,"upstream %s {" % proxy_name)
-	regex = re.compile('\s+')
-	iport_list=regex.split(iport)
-	l=len(iport_list)
-	for i in range(l):
-	    lines.insert(16+i,"	server %s;\n" % iport_list[i-1])
+	portiner_re=re.compile('^portainer\:[1-9]\d{3,6}$')
+	pm=portiner_re.match(iport)
+	if pm:
+	    ps_list=iport.split(':')
+	    portiner_port=ps_list[1]
+	    portiner_ip=['10.11.3.123','10.11.3.125','10.11.3.126','10.11.3.127']
+	    ll=len(portiner_ip)
+	    for i in range(ll):
+	        lines.insert(16+i,"	server %s:%s;\n" % (portiner_ip[i-1],portiner_port))
+	else:    
+	    regex = re.compile('\s+')
+	    iport_list=regex.split(iport)
+	    l=len(iport_list)
+	    for i in range(l):
+	        lines.insert(16+i,"	server %s;\n" % iport_list[i-1])
 	wfile=open('%s' % d_file,"w+")
 	for ii in lines:
 	    wfile.write(ii)
--- red-noportainer.py
+++ red-noportainer.py
@@ -1,7 +1,7 @@
 import re
 import os
 domain_regex=re.compile(r'^[\w-]+\.\w+\.(com|cn|io|net|org)$')
-iport_regex=re.compile('^((2[0-4][0-9]|25[0-5]|1[0-9][0-9]|[1-9]?[0-9])(\.(2[0-4][0-9]|25[0-5]|1[0-9][0-9]|[1-9]?[0-9])){3}|portainer)\:[1-9]\d{3,6}$')
+iport_regex=re.compile('^(2[0-4][0-9]|25[0-5]|1[0-9][0-9]|[1-9]?[0-9])(\.(2[0-4][0-9]|25[0-5]|1[0-9][0-9]|[1-9]?[0-9])){3}\:[1-9]\d{3,6}$')
 
 def domain_status(domain):
     m=domain_regex.match(domain)
@@ -12,7 +12,6 @@ def domain_status(domain):
 def iport_status(iport):
     regex = re.compile('\s+')
     iport_list=regex.split(iport)
-    print iport_list
     l=len(iport)
     for i in range(l):
         im=iport_regex.match(iport_list[i])
@@ -20,8 +19,5 @@ def iport_status(iport):
 	    spt=int(im.group().split(':')[1])
 	    if 20000 > spt > 8000:
 		    return "iportyes"
-	    else:
-		   print "port limit err"
-		   return "iportno"
         else:
 	    return "iportno"
